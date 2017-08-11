0fbd38cf5823d611acedbe1bab01e960d8e0786d
willm <willmurdoch@gmail.com>
2017-08-10 21:46:56 -0400

    Lots of documentation & it is actually organized now

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-var camera, scene, renderer, objects = [];
+var camera, scene, renderer;
 function init() {
-	// scene
 	scene = new THREE.Scene();
-	var gui = new dat.GUI();
-	var clock = new THREE.Clock();
-
-	var enableFog = true;
-	if (enableFog)
-		scene.fog = new THREE.FogExp2('rgb(0, 0, 0)', 0.005);
-
 	scene.background = new THREE.Color('#000');
 
+	//GUI for changing properties with sliders for testing
+	var gui = new dat.GUI();
+
+	//Fog for keeping plane edges hidden
+	var enableFog = true;
+	if (enableFog) scene.fog = new THREE.FogExp2('rgb(0, 0, 0)', 0.005);
+
 	//Add lighting
-	var myLight = getSpotLight(1, 'rgb(255, 255, 255)');
+	var myLight = getSpotLight(1);
 	myLight.position.x = 0;
 	myLight.position.y = 29;
 	myLight.position.z = 41;
 
-	// load the environment map
+	//Environment map for generating image-based reflections
 	var path = 'assets/cubemap/';
 	var format = '.jpg';
 	var urls = [
@@ -28,16 +27,14 @@ function init() {
 	var reflectionCube = new THREE.CubeTextureLoader().load(urls);
 
 	//Add floor
-	var planeMaterial = getMaterial('standard', 'rgb(255, 255, 255)');
+	var planeMaterial = getMaterial('standard');
 	var plane = getPlane(planeMaterial, 1030);
-	plane.name = 'plane-1';
+	plane.name = 'floor';
 	plane.rotation.x = Math.PI/2;
 
 	//Floor textures
 	var planeLoader = new THREE.TextureLoader();
-	planeMaterial.map = planeLoader.load('assets/textures/metalFloor.jpg');
-	planeMaterial.bumpMap = planeLoader.load('assets/textures/metalFloor.jpg');
-	planeMaterial.roughnessMap = planeLoader.load('assets/textures/metalFloor.jpg');
+	planeMaterial.map = planeMaterial.bumpMap = planeMaterial.roughnessMap = planeLoader.load('assets/textures/metalFloor.jpg');
 	planeMaterial.envMap = reflectionCube;
 	planeMaterial.roughness = 0.7;
 	planeMaterial.metalness = 0.2;
@@ -52,49 +49,30 @@ function init() {
 		texture.repeat.set(85, 85);
 	});
 
-	//Camera
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
-	camera.position.x = 8.65;
-	camera.position.y = 13.97;
-	camera.position.z = 24.76;
-
-	//Renderer
-	renderer = new THREE.WebGLRenderer();
-	renderer.shadowMap.enabled = true;
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setClearColor('rgb(220, 220, 220)');
-	document.getElementById('webgl').appendChild(renderer.domElement);
-
-	//Load external geometry
+	//Load external model
 	var loader = new THREE.OBJLoader();
 	var textureLoader = new THREE.TextureLoader();
-
 	loader.load('assets/models/r2d2/r2-d2.obj', function (object) {
 		var colorMap = textureLoader.load('assets/models/r2d2/R2D2_Diffuse.jpg');
 		var reflectionMap = textureLoader.load('assets/models/r2d2/R2D2_Reflection.jpg');
-		var faceMaterial = getMaterial('standard', 'rgb(255, 255, 255)');
+		var myMaterial = getMaterial('standard');
 
-		object.traverse(function(child) {
-			if (child.name == 'Merged_Meshes') {
-				child.material = faceMaterial;
+		//Combine sub-objects of import into a group and give them their properties
+		object.traverse(function(child){
+			if (child.name == 'Merged_Meshes'){
+				child.material = myMaterial;
 				child.castShadow = true;
-				faceMaterial.roughness = 0.28;
-				faceMaterial.envMap = reflectionCube;
-				faceMaterial.map = colorMap;
-				faceMaterial.metalness = 0.15;
-				faceMaterial.bumpScale = 0.175;
-
-				//Controls for imported model
-				var folder3 = gui.addFolder('R2D2');
-				folder3.add(faceMaterial, 'roughness', 0, 1);
-				folder3.add(faceMaterial, 'metalness', 0, 1);
+				myMaterial.roughness = 0.28;
+				myMaterial.envMap = reflectionCube;
+				myMaterial.map = colorMap;
+				myMaterial.metalness = 0.15;
+				myMaterial.bumpScale = 0.175;
 			}
 		});
-		object.scale.x = 0.1;
-		object.scale.y = 0.1;
-		object.scale.z = 0.1;
+
+		//Adjust and store final object
+		object.scale.x = object.scale.y = object.scale.z = 0.1;
 		object.name = 'r2';
-		objects.push(object);
 		scene.add(object);
 	});
 
@@ -102,36 +80,56 @@ function init() {
 	scene.add(plane);
 	scene.add(myLight);
 
-	// controls
+	//Create and position camera
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 1000);
+	camera.position.x = 8.65;
+	camera.position.y = 13.97;
+	camera.position.z = 24.76;
+
+	//Set up and assign renderer
+	renderer = new THREE.WebGLRenderer();
+	renderer.shadowMap.enabled = true;
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor('rgb(220, 220, 220)');
+	document.getElementById('webgl').appendChild(renderer.domElement);
+
+	//Controls for slider-based manipulation
 	var controls = new THREE.OrbitControls(camera, renderer.domElement);
-	controls.minPolarAngle = 0; // radians
-	controls.maxPolarAngle = Math.PI / 2 - 0.01; // radians
-	controls.target = new THREE.Vector3(0, 7, 0); //rotate camera
 
+	//Light controls
 	var folder1 = gui.addFolder('light');
-	folder1.add(myLight.position, 'x', 0, 360);
-	folder1.add(myLight.position, 'y', 0, 360);
-	folder1.add(myLight.position, 'z', 0, 360);
+	var folder1Props = ['x', 'y', 'z'];
+	addControls(folder1, myLight.position, folder1Props, 0, 360);
 
+	//Mesh controls
 	var folder2 = gui.addFolder('floor');
-	folder2.add(planeMaterial, 'roughness', 0, 1);
-	folder2.add(planeMaterial, 'metalness', 0, 1);
+	var folder2Props = ['roughness', 'metalness'];
+	addControls(folder2, planeMaterial, folder2Props, 0, 1);
 
-	update(renderer, scene, camera, controls);
-
-	//Resize canvas for responsiveness
-	window.addEventListener('resize', onWindowResize, false);
-	function onWindowResize(){
-	  camera.aspect = window.innerWidth / window.innerHeight;
-	  camera.updateProjectionMatrix();
-	  renderer.setSize( window.innerWidth, window.innerHeight );
-	}
+	//Constrain control camera so it doesn't rotate underground
+	controls.minPolarAngle = 0;
+	controls.maxPolarAngle = Math.PI / 2 - 0.01;
+	controls.target = new THREE.Vector3(0, 7, 0);
 
 	//Return initial state
+	update(renderer, scene, camera, controls);
 	return scene;
 }
 
-//Bindings
+//Add controls for testing
+function addControls(folder, object, properties, rangeIn, rangeOut){
+	properties.forEach(function(property){
+		folder.add(object, property, rangeIn, rangeOut);
+	});
+}
+
+function setAttributes(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+
+//Easy bindings for desktop & mobile
 function bindElem(target, getParent, animation){
 	var raycaster = new THREE.Raycaster();
 	var mouse = new THREE.Vector2();
@@ -159,81 +157,72 @@ function bindElem(target, getParent, animation){
 		}
 	}
 }
-bindElem('r2', true, function(){
-	var animTarget = scene.getObjectByName('r2');
-	new TWEEN.Tween({val: 0}).to({val: 5}, 150).onUpdate(function(){
-		animTarget.position.y = this.val;
-	}).start();
-	new TWEEN.Tween({val: 5}).to({val: 0}, 150).delay(150).onUpdate(function(){
-		animTarget.position.y = this.val;
-	}).start();
-});
 
-function getSpotLight(intensity, color) {
-	color = color === undefined ? 'rgb(255, 255, 255)' : color;
-	var light = new THREE.SpotLight(color, intensity);
-	light.castShadow = true;
+//Resize canvas for responsiveness
+window.addEventListener('resize', onWindowResize, false);
+function onWindowResize(){
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+//Lighting
+function getSpotLight(intensity, color = 'rgb(255, 255, 255)'){
+	var light = new THREE.SpotLight(color, intensity), color = color;
 	light.penumbra = 0.5;
-	var shadowMapSize = 550;
 
-	//Set up shadow properties for the light
-	light.shadow.mapSize.width = 4096;  // default: 512
-	light.shadow.mapSize.height = 4096; // default: 512
+	//Shadow configuration
+	var shadowMapSize = 550;
+	light.castShadow = true;
+	light.shadow.mapSize.width = light.shadow.mapSize.height = 4096;
 	light.shadow.bias = 0.001;
 
-	light.shadow.camera.left = -shadowMapSize;
-	light.shadow.camera.bottom = -shadowMapSize;
-	light.shadow.camera.right = shadowMapSize;
-	light.shadow.camera.top = shadowMapSize;
+	//Range for casting shadows, keep as low as possible
+	light.shadow.camera.left = light.shadow.camera.bottom = -shadowMapSize;
+	light.shadow.camera.right = light.shadow.camera.top = shadowMapSize;
 
 	return light;
 }
-
 function getDirectionalLight() {
 	var light = new THREE.myLight(0xffffff, 1.5);
-	light.castShadow = true;
+
+	//Shadow configuration
 	var shadowMapSize = 100;
+	light.castShadow = true;
+	light.shadow.mapSize.width = light.shadow.mapSize.height = 4096;
+	light.shadow.bias = 0.001;
 
-	//Set up shadow properties for the light
-	light.shadow.mapSize.width = 4096;
-	light.shadow.mapSize.height = 4096;
-	light.shadow.bias = 0.0001;
-
-	//Adjust shadow threshold
-	light.shadow.camera.left = -shadowMapSize;
-	light.shadow.camera.bottom = -shadowMapSize;
-	light.shadow.camera.right = shadowMapSize;
-	light.shadow.camera.top = shadowMapSize;
+	//Range for casting shadows, keep as low as possible
+	light.shadow.camera.left = light.shadow.camera.bottom = -shadowMapSize;
+	light.shadow.camera.right = light.shadow.camera.top = shadowMapSize;
 
 	return light;
 }
 
-function getBox(w, h, d) {
-	var geometry = new THREE.BoxGeometry(w, h, d);
-	var material = new THREE.MeshPhongMaterial({
-		color: 'rgb(120, 120, 120)',
-	});
-	var obj = new THREE.Mesh(geometry, material);
-	obj.castShadow = true;
+/****Basic Geometry****/
+//
 
+//Create a basic cube
+function getBox(w, h, d, material) {
+	var geometry = new THREE.BoxGeometry(w, h, d);
+	var myMaterial = material;
+	var obj = new THREE.Mesh(geometry, myMaterial);
+	obj.castShadow = true;
 	return obj;
 }
-
+//Create a basic plane
 function getPlane(material, size) {
 	var geometry = new THREE.PlaneGeometry(size, size);
 	material.side = THREE.DoubleSide;
 	var obj = new THREE.Mesh(geometry, material);
 	obj.receiveShadow = true;
-
 	return obj;
 }
 
-function getMaterial(type, color) {
+//Predefine basic materials for ease of access
+function getMaterial(type, myColor = 'rgb(255, 255, 255)') {
 	var selectedMaterial;
-	var materialOptions = {
-		color: color === undefined ? 'rgb(255, 255, 255)' : color,
-	};
-
+	var materialOptions = { color: myColor };
 	switch (type) {
 		case 'basic':
 			selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
@@ -251,9 +240,10 @@ function getMaterial(type, color) {
 			selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
 			break;
 	}
-
 	return selectedMaterial;
 }
+
+//Re-render scene
 function update(renderer, scene, camera, controls) {
 	renderer.render(scene, camera);
 	controls.update();
@@ -263,4 +253,14 @@ function update(renderer, scene, camera, controls) {
 	});
 }
 
+//Start everything up and apply bindings
 var scene = init();
+bindElem('r2', true, function(){
+	var animTarget = scene.getObjectByName('r2');
+	new TWEEN.Tween({val: 0}).to({val: 5}, 150).onUpdate(function(){
+		animTarget.position.y = this.val;
+	}).start();
+	new TWEEN.Tween({val: 5}).to({val: 0}, 150).delay(150).onUpdate(function(){
+		animTarget.position.y = this.val;
+	}).start();
+});
